19de4a6599631e52ac900dd13017ffaf0ee390b4
Igna-M1023 <imagnarelli@etrr.edu.ar>
2024-05-24 14:20:32 -0300

    Ejercicio 13

--- Practica_Ejercicios/13-Main.c
+++ Practica_Ejercicios/13-Main.c
@@ -1,2 +1,11 @@
 #include <stdio.h> 
 
+int main(void){
+    int preciokg;
+    float cantidadkg;
+    printf("Ingrese el valor del kg del producto: ");
+    scanf("%d", &preciokg);
+    printf("Ingrese la cantidad de mercancia comprada (en kg): ");
+    scanf("%f", &cantidadkg);
+    printf("El valor a pagar es: %d", preciokg*cantidadkg);
+}
