9dfc5e9a4f6d514acef0032e907d5fe9905a1e9c
Igna-M1023 <imagnarelli@etrr.edu.ar>
2024-07-03 14:22:12 -0300

    45- arreglar codigo 46- tabla de multiplicar 47- sin terminar 48- sin terminar

--- Practica_Ejercicios/45-Main.c
+++ Practica_Ejercicios/45-Main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-/* int main(void){
+int main(void){
     int numing,numcal;
-    for(numing=0;numing>10;numing++){
+    for(numing=0;numing<10;numing++){
         printf("Ingrese un numero: ");
         scanf("%d", &numcal);
-        if(numcal>=0) printf("Este numero es un positivo");
+        if(numcal>=0) printf("Este numero es un positivo\n");
     }
     return 0;
 }
-*/
---
+++ Practica_Ejercicios/47-Main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+int main(void){
+    int buce,total;
+    int numb=0;
+    while(numb>0){
+        buce++;
+        total+=numb;
+        printf("Ingrese un numero: ");
+        scanf("%d", &numb);
+    }
+    printf("El promedio es: %d", total/buce);
+    return 0;
+}    
+*/
