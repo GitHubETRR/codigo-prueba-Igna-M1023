204b8bbd52fcb8d9b1a6fe6fc00b8e1d9f76448e
Igna-M1023 <imagnarelli@etrr.edu.ar>
2024-08-09 14:40:09 -0300

    3 - Funcion entre 10 y 20 numero random y devolver numero mayor (Pasar a funcion)

--- Extra/2-ArrayMain.c
+++ Extra/2-ArrayMain.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define liminf 10
+#define limupp 20
+#define tamano 50
+
+
+// pasar a funcion
 
 int main(void){
+    srand(time(NULL));
+    int VectorA[tamano];
+    int cantm;
+    for(int i=0;i<tamano;i++){
+        VectorA[i] = rand()%(liminf-limupp+1)+20;
+        printf("%d\n", VectorA[i]);
+        if(VectorA[i]==20) cantm++;
+    }
+    printf("El numero mayor %d se repitio %d veces", limupp,cantm);
     return 0;
+    
 }
