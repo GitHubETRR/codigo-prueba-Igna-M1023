f2da4961d806a4a4c9b74d1b7877510932933ab2
Igna-M1023 <imagnarelli@etrr.edu.ar>
2024-09-27 11:42:35 -0300

    modvil

--- Extra/HeroeVillanoStruct.c
+++ Extra/HeroeVillanoStruct.c
@@ -2,15 +2,20 @@
 #include <string.h>
 #define LARGE 32
 #define BIOLARGE 64
-#define VIDAINI 100
-#define VIDAMAX 150
-
+#define VIDAININ 100
+#define VIDAMAXN 150
+#define VIDAINIR 100
+#define VIDAMAXR 150
+#define DANOBN 10
+#define DANOBR 15
+// ataque basico
 typedef struct{
     char nombre[LARGE];
     float dano;
     float dano2;
     float salud;
     float salud2;
+    float multdano;
     int usos;
 } caracteristicas_t;  
 
@@ -19,6 +24,7 @@ typedef struct{
     char bio[BIOLARGE];
     float vida;
     float vidamax;
+    int danobasico;
     caracteristicas_t danio1;
     caracteristicas_t danio2;
     caracteristicas_t danio3;
@@ -33,25 +39,123 @@ int main(void){
     personaje_t nikitox = {
     "Nikitox",
     "Nikitox es un adolescente...",
-    VIDAINI,
-    VIDAMAX,
-    {
-        "Ataque Choripan",
-        50,
-        100,
-        0,
-        0,
-        3
-    }
+    VIDAININ,
+    VIDAMAXN,
+    DANOBN,
+        {
+            "Volar",
+            0,
+            0,
+            5,
+            15,
+            0,
+            4
+        },
+        {
+            "Ataque de ira",
+            0,
+            0,
+            50,
+            60,
+            2.5,
+            2
+        },
+        {
+            "SuperFuerza",
+            0,
+            0,
+            70,
+            75,
+            3,
+            1
+        },
+        {
+            "Aleteo",
+            20,
+            30,
+            0,
+            0,
+            0,
+            4
+        },
+        {
+            "Patada 360°",
+            40,
+            45,
+            0,
+            0,
+            0,
+            3
+        },
+        {
+            "Corte con tramontina",
+            55,
+            65,
+            0,
+            0,
+            0,
+            2
+        }
     };
-    printf("%s\n", nikitox.nombre);
-    printf("%s\n", nikitox.bio);
-    printf("%.2f\n", nikitox.vida);
-    printf("%.2f\n", nikitox.vidamax);
-    printf("%s\n", nikitox.danio1.nombre);
-    printf("%.2f\n", nikitox.danio1.dano);
-    printf("%.2f\n", nikitox.danio1.dano2);
-    printf("%.2f\n", nikitox.danio1.salud);
-    printf("%.2f\n", nikitox.danio1.salud2);
-    printf("%d\n", nikitox.danio1.usos);
+    personaje_t Ruby = {
+    "Ruby",
+    "Rubén Larreta hijo de Horacio Larreta, a temprana edad despertó un sentimiento de justicia que se apodero de su mente.",
+    VIDAINIR,
+    VIDAMAXR,
+    DANOBR,
+        {
+            "Tomar café",
+            0,
+            0,
+            15,
+            45,
+            1.1,
+            3
+        },
+        {
+            "Comer un grido familiar",
+            0,
+            0,
+            60,
+            75,
+            0,
+            1
+        },
+        {
+            "Colocar panel solar",
+            0,
+            0,
+            20,
+            40,
+            2,
+            2
+        },
+        {
+            "Tirarte un sobre de azúcar",
+            10,
+            25,
+            0,
+            0,
+            0,
+            4
+        },
+        {
+            "Conectarte a 220V",
+            50,
+            65,
+            0,
+            5,
+            0,
+            2
+        },
+        {
+            "Tirarte café",
+            20,
+            35,
+            0,
+            0,
+            0,
+            3
+        }
+    };
 }
--- Extra/SDL.c
+++ Extra/SDL.c
@@ -1,52 +1,138 @@
-//Using SDL and standard IO
-#include <SDL.h>
-#include <stdio.h>
-
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-int main( int argc, char* args[] )
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+ 
+int main(int argc, char *argv[])
 {
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
-    
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
-
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+ 
+    // returns zero on success else non-zero
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("error initializing SDL: %s\n", SDL_GetError());
     }
-        else
-    {
-        //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
-        {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+    SDL_Window* win = SDL_CreateWindow("GAME", // creates a window
+                                       SDL_WINDOWPOS_CENTERED,
+                                       SDL_WINDOWPOS_CENTERED,
+                                       1000, 1000, 0);
+ 
+    // triggers the program that controls
+    // your graphics hardware and sets flags
+    Uint32 render_flags = SDL_RENDERER_ACCELERATED;
+ 
+    // creates a renderer to render our images
+    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
+ 
+    // creates a surface to load an image into the main memory
+    SDL_Surface* surface;
+ 
+    // please provide a path for your image
+    surface = IMG_Load("path");
+ 
+    // loads image to our graphics hardware memory.
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface);
+ 
+    // clears main-memory
+    SDL_FreeSurface(surface);
+ 
+    // let us control our image position
+    // so that we can move it with our keyboard.
+    SDL_Rect dest;
+ 
+    // connects our texture with dest to control position
+    SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h);
+ 
+    // adjust height and width of our image box.
+    dest.w /= 6;
+    dest.h /= 6;
+ 
+    // sets initial x-position of object
+    dest.x = (1000 - dest.w) / 2;
+ 
+    // sets initial y-position of object
+    dest.y = (1000 - dest.h) / 2;
+ 
+    // controls animation loop
+    int close = 0;
+ 
+    // speed of box
+    int speed = 300;
+ 
+    // animation loop
+    while (!close) {
+        SDL_Event event;
+ 
+        // Events management
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+ 
+            case SDL_QUIT:
+                // handling of close button
+                close = 1;
+                break;
+ 
+            case SDL_KEYDOWN:
+                // keyboard API for key pressed
+                switch (event.key.keysym.scancode) {
+                case SDL_SCANCODE_W:
+                case SDL_SCANCODE_UP:
+                    dest.y -= speed / 30;
+                    break;
+                case SDL_SCANCODE_A:
+                case SDL_SCANCODE_LEFT:
+                    dest.x -= speed / 30;
+                    break;
+                case SDL_SCANCODE_S:
+                case SDL_SCANCODE_DOWN:
+                    dest.y += speed / 30;
+                    break;
+                case SDL_SCANCODE_D:
+                case SDL_SCANCODE_RIGHT:
+                    dest.x += speed / 30;
+                    break;
+                default:
+                    break;
+                }
+            }
         }
-                else
-        {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
-
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
-
-            //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
-        }
+ 
+        // right boundary
+        if (dest.x + dest.w > 1000)
+            dest.x = 1000 - dest.w;
+ 
+        // left boundary
+        if (dest.x < 0)
+            dest.x = 0;
+ 
+        // bottom boundary
+        if (dest.y + dest.h > 1000)
+            dest.y = 1000 - dest.h;
+ 
+        // upper boundary
+        if (dest.y < 0)
+            dest.y = 0;
+ 
+        // clears the screen
+        SDL_RenderClear(rend);
+        SDL_RenderCopy(rend, tex, NULL, &dest);
+ 
+        // triggers the double buffers
+        // for multiple rendering
+        SDL_RenderPresent(rend);
+ 
+        // calculates to 60 fps
+        SDL_Delay(1000 / 60);
     }
-        //Destroy window
-    SDL_DestroyWindow( window );
-
-    //Quit SDL subsystems
+ 
+    // destroy texture
+    SDL_DestroyTexture(tex);
+ 
+    // destroy renderer
+    SDL_DestroyRenderer(rend);
+ 
+    // destroy window
+    SDL_DestroyWindow(win);
+     
+    // close SDL
     SDL_Quit();
-
+ 
     return 0;
 }
